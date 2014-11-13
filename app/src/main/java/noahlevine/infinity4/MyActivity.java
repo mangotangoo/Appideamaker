aef77651bdb8280e3a6533385b392559b57568de
mangotangoo <noahsonic@hotmail.com>
2014-11-12 23:24:12 -0500

    fixed the strings and the output

--- app/src/main/java/noahlevine/infinity4/MyActivity.java
+++ app/src/main/java/noahlevine/infinity4/MyActivity.java
@@ -1,16 +1,20 @@
 /*App idea maker 1.0 alpha
  * By: Noah Levine & Jonah Stegman
+ *
+ * "Awesome","Queen","King","Cow","Plum","Pear","Orange","Apple","Musical","Music","Electric","TV","Football","Desk","Soccer","Basketball","Science", "Math","Teacher","School","Money","Movie","Light Saber","Light","Garbage","Sassy","Wall","Hat","Pants","Shirt","Fruit", "Horse" , "Monkey", "Computer", "Tablet", "Phone", "Dog", "Cat", "Banana", "Hair", "Mouse", "Alien" };
+   {"","Painter","Trivia","Robber","Suggestor","Collector","Clock","Pass","Brawl","Randomizer","Rater","Destroyer" ,"Fight", "Attack", "Distracter", "Enhancer", "Social media app", "Mystery", "Identifiyer", "Ripeness", "Compare", "Measurer", "Translator"};
+
  */
 package noahlevine.infinity4;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
 
 public class MyActivity extends Activity {
     //static void main(String[] args) throws InterruptedException {
@@ -48,24 +52,42 @@ public class MyActivity extends Activity {
                     } catch (InterruptedException e1) {
                         e1.printStackTrace();
                     }
+
+                    //first set of random words
+                    String[] firstWords = getResources().getStringArray(R.array.randy);
+                    String random = firstWords[new Random().nextInt()];
+                    //second set of randomized words
+                    String[] secondWords = getResources().getStringArray(R.array.rando);
+                    String randomer = secondWords[new Random().nextInt()];
+
+                    firstTextView.setText(random + " " + randomer);
+
                 }
 
 
-                public boolean onCreateOptionsMenu(Menu menu) {
+            }
+
+
+
+         //@Override
+         public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+       // int id = item.getItemId();
+      //  if (id == R.id.action_settings) {
+        //    return true;
+      //  }
+      //  return super.onOptionsItemSelected(item);
+    //}
+});
+
         }
-        return super.onOptionsItemSelected(item);
-    }
 }
